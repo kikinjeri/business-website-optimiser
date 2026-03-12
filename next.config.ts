6fec0606b4ed09a7764657d4bb3ec04481bd633c
kikinjeri <githiimwihaki@gmail.com>
2026-03-11 22:46:50 -0400

    fixed next.config.ts and .vercelignore

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: false, // Vercel uses SWC by default
-  },
-
   reactStrictMode: true,
-  swcMinify: true,
 
-  // Vercel automatically handles image optimization
   images: {
     formats: ['image/avif', 'image/webp'],
   },
 
-  // Avoid build failures from TS or ESLint
+  // Avoid build failures from TS
   typescript: {
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 
   // Smaller bundles, faster cold starts
